fc51226b57bfa241947e42a134e09a2fee1e188e
Aaris-Kazi <aariskazi@gmail.com>
2022-06-26 19:03:23 +0530

    modification

--- app.py
+++ app.py
@@ -103,8 +103,8 @@ async def websocket_endpoint(websocket: WebSocket, client_id : str):
         
         
         
-# if __name__ == "__main__":
-#     # uvicorn.run(app, host="127.0.0.1", port = 62577)
-#     uvicorn.run(app)
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1")
+    uvicorn.run(app)
 
 # uvicorn main:app --reload
